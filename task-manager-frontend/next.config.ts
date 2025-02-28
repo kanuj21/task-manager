adffbb5419bce79ffb3f040f2c291a0cff0d6682
Dev <kanuj2151@gmail.com>
2025-02-28 08:37:51 +0530

    update next Config

--- task-manager-frontend/next.config.ts
+++ task-manager-frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+    reactStrictMode: true,
+    output: 'standalone', // Important for Vercel deployments
+
+  
 };
 
 export default nextConfig;
